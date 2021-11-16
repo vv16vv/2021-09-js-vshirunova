d5e17f1870fc289be375395cf0c9a90c23ecd3e9
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2021-11-16 13:40:59 +0300

    [javascript-05] consts: добавлена функция getFileName - вычисляет имя файла с частичными данными.

--- javascript/js-05-01.const.js
+++ javascript/js-05-01.const.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const FILES = "files"
 const FILENAME = "generated.txt"
 const MIN_NUMBER = 0
@@ -5,6 +6,11 @@ const MAX_NUMBER = 100000
 const SRC_FILE_CAPACITY = 104857600 // ~100MB
 const DST_FILE_CAPACITY = 50 * 1024 // ~50KB
 
+const getFileName = (name, order) => {
+    const srcObject = path.parse(name)
+    return `${FILES}${path.sep}${srcObject.name}.${("00000" + order).slice(-5)}${srcObject.ext}`
+}
+
 module.exports = {
-    FILES, FILENAME, MIN_NUMBER, MAX_NUMBER, SRC_FILE_CAPACITY, DST_FILE_CAPACITY
+    FILES, FILENAME, MIN_NUMBER, MAX_NUMBER, SRC_FILE_CAPACITY, DST_FILE_CAPACITY, getFileName
 }
