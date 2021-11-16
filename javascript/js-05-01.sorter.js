95a3fa373389e48857854afd10704651c7bf1d62
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2021-11-16 13:45:58 +0300

    [javascript-05] sorter: теперь функция возвращает Promise, чтобы можно было ее вызывать как последовательно, так и параллельно.

--- javascript/js-05-01.sorter.js
+++ javascript/js-05-01.sorter.js
@@ -1,27 +1,31 @@
 const {createReadStream, createWriteStream} = require("fs")
 
 async function sorter(fileName) {
-    const source = createReadStream(fileName)
-    let data = []
-    source.on("data", chunk => {
-        data = data.concat(
-            chunk.toString()
-                .split(" ")
-                .map(s => +s),
+    return new Promise(((resolve, reject) => {
+        const source = createReadStream(fileName)
+        let data = []
+        source.on("data", chunk => {
+            data = data.concat(
+                chunk.toString()
+                    .split(" ")
+                    .map(s => +s),
+            )
+        })
+        source.on("end", () => {
+                const result = data.sort((a, b) => {
+                    if (a < b) return -1
+                    else if (a > b) return 1
+                    else return 0
+                })
+                    .map(s => `${s}`)
+                    .join(" ")
+                const dst = createWriteStream(fileName)
+                dst.end(result, 'utf8')
+            resolve()
+            },
         )
-    })
-    source.on("end", () => {
-            const result = data.sort((a, b) => {
-                if (a < b) return -1
-                else if (a > b) return 1
-                else return 0
-            })
-                .map(s => `${s}`)
-                .join(" ")
-            const dst = createWriteStream(fileName)
-            dst.write(result, 'utf8')
-        },
-    )
+        source.on("error", err => reject(err))
+    }))
 }
 
 module.exports = {
