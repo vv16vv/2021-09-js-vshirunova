3e9c7b8781ffaae52b32eb6da474299a531ad764
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2022-01-29 00:17:29 +0300

    [javascript-05] js-05-01.sorter.js: corrected reading data string from the file

--- javascript/js-05-01.sorter.js
+++ javascript/js-05-01.sorter.js
@@ -1,28 +1,33 @@
 const {createReadStream, createWriteStream} = require("fs")
-const consts = require("./js-05-01.const");
+const consts = require("./js-05-01.const")
 
 async function sorter(fileName) {
     return new Promise(((resolve, reject) => {
-        const source = createReadStream(fileName)
-        let data = []
+        const source = createReadStream(fileName, {highWaterMark: consts.DST_FILE_CAPACITY})
+        let data = ""
         source.on("data", chunk => {
-            data = data.concat(
-                chunk.toString()
-                    .split(consts.NUMBER_SEPARATOR)
-                    .map(s => +s),
-            )
+            data += chunk.toString()
         })
         source.on("end", () => {
-                const result = data.sort((a, b) => {
-                    if (a < b) return -1
-                    else if (a > b) return 1
-                    else return 0
-                })
-                    .map(s => `${s}`)
-                    .join(" ")
-                const dst = createWriteStream(fileName)
-                dst.end(result, 'utf8')
-            resolve()
+                const dst = createWriteStream(fileName, {highWaterMark: consts.DST_FILE_CAPACITY})
+                data
+                    .split(consts.NUMBER_SEPARATOR)
+                    .filter(s => s !== "")
+                    .map(s => +s)
+                    .sort((a, b) => {
+                        if (a < b) return -1
+                        else if (a > b) return 1
+                        else return 0
+                    })
+                    .forEach(n => {
+                        const canWrite = dst.write(`${n}${consts.NUMBER_SEPARATOR}`, 'utf8')
+                        if (!canWrite) {
+                            dst.removeAllListeners("drain")
+                            dst.once("drain", () => dst.write(`${n}${consts.NUMBER_SEPARATOR}`))
+                        }
+                    })
+                dst.end()
+                resolve()
             },
         )
         source.on("error", err => reject(err))
