2c74b4685044307569d7545fad3186699124ed6e
Viktoria Shirunova <29532514+vv16vv@users.noreply.github.com>
2022-02-20 19:44:31 +0300

    [javascript-09] keep data in local storage

--- weather-app/src/pages/main/MainPage.tsx
+++ weather-app/src/pages/main/MainPage.tsx
@@ -4,26 +4,40 @@ import {CityInListProps} from "../../types"
 import {data} from "../../data/data"
 import {Selector} from "../../components/selector/Selector"
 import styles from "./MainPage.module.css"
+import {getCityInList, isCityInListStored, putCityInListToStorage} from "../../data/dataStorage"
 
 export const MainPage = () => {
-  const [cities, setCities] = useState<Array<CityInListProps>>([])
+  const [cities, setCities] = useState<Array<CityInListProps> | null>(null)
   useEffect(() => {
-    const cities: Array<CityInListProps> = []
-    for (let city of data) {
-      cities.push({
-        cityName: city.name,
-        country: city.country,
-        todayWeather: city.forecast[0],
+    isCityInListStored()
+      .then(value => {
+        if (value) {
+          return getCityInList()
+        } else {
+          const cities: Array<CityInListProps> = []
+          for (let city of data) {
+            cities.push({
+              cityName: city.name,
+              country: city.country,
+              todayWeather: city.forecast[0],
+            })
+          }
+          putCityInListToStorage(cities).catch(err => console.log(err))
+          return Promise.resolve(cities)
+        }
       })
-    }
-    setCities(cities)
+      .then(cities => setCities(cities))
+      .catch(err => console.log(err))
   })
 
   return <>
-    <main className="layout">
-      <div className={styles.container}>
-        <Selector data={cities}/>
-      </div>
-    </main>
+    {cities !== null ?
+      <main className="layout">
+        <div className={styles.container}>
+          <Selector data={cities}/>
+        </div>
+      </main>
+      : null
+    }
   </>
 }
